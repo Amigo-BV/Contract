7020c35826e40daed5a2f218054ebac83c1c613a
Junmo <hongjm1022@gmail.com>
2025-01-12 14:57:50 +0900

    feat: UserContract 작성

---
+++ contracts/UserContract.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract UserContract {
+    struct User {
+        address userAddress;
+        string username;
+        string profileImageCID;
+    }
+
+    mapping(address => User) public users;
+
+    event UserRegistered(address indexed userAddress, string username);
+
+    function register(
+        string memory _username,
+        string memory _profileImageCID
+    ) public {
+        require(
+            bytes(users[msg.sender].username).length == 0,
+            "User already registered"
+        );
+        users[msg.sender] = User(msg.sender, _username, _profileImageCID);
+        emit UserRegistered(msg.sender, _username);
+    }
+
+    function getUser(address _userAddress) public view returns (User memory) {
+        require(
+            bytes(users[_userAddress].username).length > 0,
+            "User not found"
+        );
+        return users[_userAddress];
+    }
+}
